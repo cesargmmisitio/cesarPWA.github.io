6f333fbc0030bc31efbf7d52bdd049db54ef847d
cesargmmisitio <82349726+cesargmmisitio@users.noreply.github.com>
2021-04-14 04:00:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,30 +5,29 @@ class MiNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
       `<a href="index.html">
-        <span
-          class="material-icons">
-          home
-        </span>
+      <span class="material-icons">
+          house
+      </span>
         Inicio
       </a>
       <a href="dispositivo.html">
         <span
           class="material-icons">
-          home_mini
+          devices
         </span>
         Dispositivo
       </a>
       <a href="historial.html">
         <span
           class="material-icons">
-          home_mini
+          view agenda
         </span>
         Historial
       </a>
       <a href="ayuda.html">
         <span
           class="material-icons">
-          help
+          info
         </span>
         Ayuda
       </a>
