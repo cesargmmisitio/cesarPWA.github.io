4f33ce204bdef2d1d2ffe7c56457de8a8c764b8d
cesargmmisitio <82349726+cesargmmisitio@users.noreply.github.com>
2021-04-14 04:44:29 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,16 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBTHnD-RQI-sgu9XqH9XyNZfi0e5SX96ng",
+  /*apiKey: "AIzaSyBTHnD-RQI-sgu9XqH9XyNZfi0e5SX96ng",
   authDomain: "controlador-iot.firebaseapp.com",
   projectId: "controlador-iot",
   storageBucket: "controlador-iot.appspot.com",
   messagingSenderId: "459085222631",
   appId: "1:459085222631:web:2a11534f1662db567f8842",
-  measurementId: "G-0VZ62QY50N"
+  measurementId: "G-0VZ62QY50N"*/
+  apiKey: "AIzaSyDmy190cntw_3Xb_cX0DlwnddGVDkWBaDs",
+    authDomain: "dmgomc-4f990.firebaseapp.com",
+    projectId: "dmgomc-4f990",
+    storageBucket: "dmgomc-4f990.appspot.com",
+    messagingSenderId: "778467266323",
+    appId: "1:778467266323:web:0d4ec30358d2c643b394e6"
 });
